33db62120c95f52f250672061a48a8b150285775
fareast1074 <88878114+fareast1074@users.noreply.github.com>
2026-07-23 15:45:11 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -873,7 +873,7 @@ function renderQCModal(auditRecord = null) {
                         <option value="UNDER REPAIR"></option>
                         <option value="OUT OF SERVICE"></option>
                         <option value="MISSING"></option>
-                        <option value="ABOLISHED"></option>
+                        <option value="DISPOSED"></option>
                     </datalist>
                 </div>
             </div>
